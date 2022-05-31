aa78effdb4d2491b7d5709f7652b4bd5a04d7da8
Deniz Can Cigsar <denizcan@arselon.com>
2022-05-31 17:58:20 +0300

    Issue-1: nuggeted project

---
+++ src/nugeted/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CommandLine;
+
+namespace nugeted
+{
+    class Options
+    {
+        [Option('s', "source", Required = true, HelpText = "Source path")]
+        public string Source { get; set; }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            CommandLine.Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>(o =>
+                {
+                    Console.WriteLine(o.Source);
+
+                });
+        }
+    }
+}
