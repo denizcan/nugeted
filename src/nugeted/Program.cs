0ea77befffc499ad9ca818c041b9322cd8e81c7a
Deniz Can Cigsar <denizcan@arselon.com>
2022-05-31 18:50:24 +0300

    Issue 1: FileSystemWatcher is added

--- src/nugeted/Program.cs
+++ src/nugeted/Program.cs
@@ -17,9 +17,23 @@ namespace nugeted
             CommandLine.Parser.Default.ParseArguments<Options>(args)
                 .WithParsed<Options>(o =>
                 {
-                    Console.WriteLine(o.Source);
+                    var path = Path.GetDirectoryName(o.Source);
+                    var watcher = new FileSystemWatcher(path);
+                    watcher.Filter = "*.dll";
+                    watcher.NotifyFilter = NotifyFilters.LastWrite;
+                    watcher.Changed += Watcher_Changed;
+                    watcher.Created += Watcher_Changed;
+                    watcher.Renamed += Watcher_Changed;
+                    watcher.Deleted += Watcher_Changed;
+                    watcher.EnableRaisingEvents = true;
 
+                    Console.ReadLine();
                 });
+        }
+
+        private static void Watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"{e.ChangeType} {e.Name} {File.GetLastWriteTime(e.FullPath)} {File.GetAttributes(e.FullPath)}");
         }
     }
 }
